6fe806d84f3763451b9b2c1a4c3cde4dac044ac7
Lillie Mackey <lamackey@cps.edu>
2023-12-07 10:38:49 -0600

    Changes

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -9,14 +9,21 @@ x = data[["miles(000)","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
-
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 #create linear regression model
-
+model = LinearRegression().fit(xtrain, ytrain)
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
-
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y),2)
+print(coef)
+print(intercept)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
+
 print("***************")
 print("Testing Results")
+print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2+ {intercept}")
+print("R_squared value:", r_squared)
