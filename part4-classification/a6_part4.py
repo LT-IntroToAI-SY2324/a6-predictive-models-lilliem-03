cbfa303c4f585793a81d10c8364ff288bd70e4d3
Lillie Mackey <lamackey@cps.edu>
2023-12-13 10:31:45 -0600

    Finsiehd 3 and 4

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -3,6 +3,7 @@ from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+
 data = pd.read_csv("part4-classification/suv_data.csv")
 data['Gender'].replace(['Male','Female'],[0,1],inplace=True)
 
@@ -20,7 +21,6 @@ x = scaler.transform(x)
 # Step 4: Split the data into training and testing data
 x_train, x_test, y_train, y_test = train_test_split(x, y)
 # Step 5: Fit the data
-
 # Step 6: Create a LogsiticRegression object and fit the data
 model = linear_model.LogisticRegression().fit(x_train, y_train)
 # Step 7: Print the score to see the accuracy of the model
@@ -53,3 +53,8 @@ for index in range(len(x_test)):
 
     print("Predicted Species: " + y_pred + " Actual Species: " + actual)
     print("")
+
+car_data = [[34, 56000]]
+car_data_scaled = scaler.transform(car_data)
+my_prediction = model.predict(car_data)
+print(my_prediction)
